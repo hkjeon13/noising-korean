387bed40479b631e83b49c87f5c85dd7300ec0a2
hkjeon13 <hkjeon13@gmail.com>
2020-11-30 10:27:29 +0900

    remove idea folder

--- noising/noise_generator.py
+++ noising/noise_generator.py
@@ -1,120 +1,127 @@
 import re
 import random
 
+consonant = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+vowel = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ',
+         'ㅢ', 'ㅣ']
+final_consonant = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ',
+                   'ㅂ',
+                   'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+pairs = {'ㅏ': 'ㅑ', 'ㅑ': 'ㅏ', 'ㅓ': 'ㅕ', 'ㅕ': 'ㅓ', 'ㅗ': 'ㅛ', 'ㅛ': 'ㅗ', 'ㅜ': 'ㅠ', 'ㅠ': 'ㅜ', }
 
-class NoiseGenerator(object):
-    def __init__(self):
-        self.consonant = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
-        self.vowel = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ',
-                      'ㅢ', 'ㅣ']
-        self.final_consonant = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ',
-                                'ㅂ',
-                                'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
-        self.exceptions = ['ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅗ']
-        self.pairs = {'ㅏ': 'ㅑ', 'ㅑ': 'ㅏ', 'ㅓ': 'ㅕ', 'ㅕ': 'ㅓ', 'ㅗ': 'ㅛ', 'ㅛ': 'ㅗ', 'ㅜ': 'ㅠ', 'ㅠ': 'ㅜ', }
+oral_consonant = ['ㄱ', 'ㄷ', 'ㄹ', 'ㅂ', 'ㅅ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅎ']
+nasal_consonant = ['ㅁ', 'ㄴ', 'ㅇ']
+liquid_consonant = ['ㄹ']
 
-        self.formal_morpheme = [self.jamo_split(mor) for mor in ['이', '을', '를', '은', '았', '었']]
-        self.oral_consonant = ['ㄱ', 'ㄷ', 'ㄹ', 'ㅂ', 'ㅅ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅎ']
-        self.nasal_consonant = ['ㅁ', 'ㄴ', 'ㅇ']
-        self.liquid_consonant = ['ㄹ']
 
-    def load_pairs(self, path):
-        with open(path, 'r', encoding='utf-8') as r:
-            contents = [l.split('\t') for l in r.read().split('\n')]
-        dictionary = {k: v for k, v in contents if k != v and k.strip() != ''}
-        return dictionary
+def load_pairs(path):
+    with open(path, 'r', encoding='utf-8') as r:
+        contents = [l.split('\t') for l in r.read().split('\n')]
+    dictionary = {k: v for k, v in contents if k != v and k.strip() != ''}
+    return dictionary
 
-    def jamo_split(self, char):
-        base = ord(char) - ord('가')
-        c = base // 588
-        v = (base - 588 * c) // 28
-        f_c = base - 588 * c - 28 * v
-        return [self.consonant[c], self.vowel[v], self.final_consonant[f_c]]
 
-    def jamo_merge(self, jamo_list):
-        if jamo_list[1:] == ['', '']:
-            return jamo_list[0]
-        c, v, f_c = [_list.index(j) for _list, j in zip([self.consonant, self.vowel, self.final_consonant], jamo_list)]
-        return chr(f_c + 588 * c + 28 * v + ord('가'))
+def jamo_split(char):
+    base = ord(char) - ord('가')
+    c = base // 588
+    v = (base - 588 * c) // 28
+    f_c = base - 588 * c - 28 * v
+    return [consonant[c], vowel[v], final_consonant[f_c]]
 
-    def spliting_noise(self, content, prob=0.1):
-        condition = lambda xlist: ((xlist[-1] == ' ') and (xlist[-2] not in self.exceptions))
 
-        output = [self.jamo_split(ch) if re.match('[가-힣]', ch) else [ch, '', ''] for ch in content]
-        output = [''.join(out).strip() if condition(out) and (random.random() < prob) else content[i] for i, out in
-                  enumerate(output)]
+def jamo_merge(jamo_list):
+    if jamo_list[1:] == ['', '']:
+        return jamo_list[0]
+    c, v, f_c = [_list.index(j) for _list, j in zip([consonant, vowel, final_consonant], jamo_list)]
+    return chr(f_c + 588 * c + 28 * v + ord('가'))
 
-        return ''.join(output)
 
-    def vowel_noise(self, content, prob=0.1):
-        output = [self.jamo_split(ch) if re.match('[가-힣]', ch) else [ch, '', ''] for ch in content]
-        condition = lambda xlist: ((xlist[-1] == ' ') and (xlist[-2] in self.pairs))
-        output = [
-            self.jamo_merge([out[0], self.pairs[out[1]], out[2]]) if condition(out) and (random.random() < prob) else
-            content[i] for i, out in
-            enumerate(output)]
-        return ''.join(output)
+def splitting_noise(content, prob=0.1):
+    exceptions = ['ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅗ']
+    condition = lambda xlist: ((xlist[-1] == ' ') and (xlist[-2] not in exceptions))
 
-    def palatalization(self, fc, nc):
-        palatal = {'ㄷ': 'ㅈ', 'ㅌ': 'ㅊ'}
-        if (fc[-1] in palatal) and nc[:-1] == ['ㅇ', 'ㅣ']:
-            nc[0] = palatal[fc[-1]]
-            fc[-1] = ' '
+    output = [jamo_split(ch) if re.match('[가-힣]', ch) else [ch, '', ''] for ch in content]
+    output = [''.join(out).strip() if condition(out) and (random.random() < prob) else content[i] for i, out in
+              enumerate(output)]
+
+    return ''.join(output)
+
+
+def vowel_noise(content, prob=0.1):
+    output = [jamo_split(ch) if re.match('[가-힣]', ch) else [ch, '', ''] for ch in content]
+    condition = lambda xlist: ((xlist[-1] == ' ') and (xlist[-2] in pairs))
+    output = [
+        jamo_merge([out[0], pairs[out[1]], out[2]]) if condition(out) and (random.random() < prob) else
+        content[i] for i, out in
+        enumerate(output)]
+    return ''.join(output)
+
+
+def palatalization(fc, nc):
+    palatal = {'ㄷ': 'ㅈ', 'ㅌ': 'ㅊ'}
+    if (fc[-1] in palatal) and nc[:-1] == ['ㅇ', 'ㅣ']:
+        nc[0] = palatal[fc[-1]]
+        fc[-1] = ' '
+    return fc, nc
+
+
+def linking(fc, nc):
+    formal_morpheme = [jamo_split(mor) for mor in ['이', '을', '를', '은', '았', '었', '아', '어']]
+    links = {'ㄻ': 'ㄹㅁ', 'ㅄ': 'ㅂㅆ', 'ㄳ': 'ㄱㅅ', 'ㄽ': 'ㄹㅅ', 'ㅊ': ' ㅊ', 'ㅂ': ' ㅂ', 'ㅍ': ' ㅂ', 'ㄷ':' ㄹ', 'ㄹ': ' ㄹ'}
+    if (fc[-1] in links) and (nc in formal_morpheme):
+        fc[-1], nc[0] = links[fc[-1]]
+    return fc, nc
+
+
+def liquidization(fc, nc):
+    liquid_set = {'ㄴㄹ': 'ㄹㄹ', 'ㄹㄴ': 'ㄹㄹ', 'ㄾㄴ': 'ㄹㄹ'}
+    exception_set = {'ㄴㄹㅕㄱ': 'ㄴㄴ'}
+
+    if fc[-1] + ''.join(nc) in exception_set:
+        fc[-1], nc[0] = exception_set[fc[-1] + ''.join(nc)]
+        return fc, nc
+    else:
+        if fc[-1] + nc[0] in liquid_set:
+            fc[-1], nc[0] = liquid_set[fc[-1] + nc[0]]
         return fc, nc
 
-    def linking(self, fc, nc):
-        links = {'ㄻ': 'ㄹㅁ', 'ㅄ': 'ㅂㅆ', 'ㄳ': 'ㄱㅅ', 'ㄽ': 'ㄹㅅ', 'ㅊ': ' ㅊ', 'ㅂ':' ㅂ', 'ㅍ': ' ㅂ'}
-        if (fc[-1] in links) and (nc in self.formal_morpheme):
-            fc[-1], nc[0] = links[fc[-1]]
-        return fc, nc
 
-    def liquidization(self, fc, nc):
-        liquid_set = {'ㄴㄹ': 'ㄹㄹ', 'ㄹㄴ': 'ㄹㄹ', 'ㄾㄴ':'ㄹㄹ'}
-        exception_set = {'ㄴㄹㅕㄱ':'ㄴㄴ'}
-
-        if fc[-1]+''.join(nc) in exception_set:
-            fc[-1], nc[0] = exception_set[fc[-1]+''.join(nc)]
-            return fc, nc
-        else:
-            if fc[-1] + nc[0] in liquid_set:
-                fc[-1], nc[0] = liquid_set[fc[-1] + nc[0]]
-            return fc, nc
-
-    def nasalization(self, fc, nc):
-        nasalization_set = {'ㅂㅁ': 'ㅁㅁ', 'ㄷㄴ': 'ㄴㄴ', 'ㄱㅁ': 'ㅇㅁ', 'ㄱㄴ': 'ㅇㄴ', 'ㅇㄹ': 'ㅇㄴ',
-                            'ㅁㄹ': 'ㅁㄴ', 'ㄲㄴ': 'ㅇㄴ', 'ㄱㄹ': 'ㅇㄴ', 'ㅂㄹ': 'ㅁㄴ', 'ㄱㄹ': 'ㅇㄴ',
-                            'ㅊㄹ': 'ㄴㄴ', 'ㄺㄴ': 'ㅇㄴ', 'ㅍㄴ': 'ㅁㄴ'}
-        fc_c = fc[-1] + nc[0]
-        if fc_c in nasalization_set:
-            fc[-1], nc[0] = nasalization_set[fc_c]
-        return fc, nc
-
-    def assimilation(self, fc, nc):
-        # assimilation not employed in the nasalization function. each other has the similar rules.
-        reverse_assimil = {'ㄺㄴ':'ㅇㄴ'}
-        fc_c = fc[-1] + nc[0]
-        if fc_c in reverse_assimil:
-            fc[-1], nc[0] = reverse_assimil[fc_c]
-        return fc, nc
-
-    def phonological_process(self, content, prob=0.3):
-        uncased = [self.jamo_split(ch) if re.match('[가-힣]', ch) else [ch, '', ''] for ch in content]
-
-        for i in range(len(uncased) - 1):
-            if random.random() < prob:
-                uncased[i], uncased[i + 1] = self.palatalization(uncased[i], uncased[i + 1])
-                uncased[i], uncased[i + 1] = self.linking(uncased[i], uncased[i + 1])
-                uncased[i], uncased[i + 1] = self.liquidization(uncased[i], uncased[i + 1])
-                uncased[i], uncased[i + 1] = self.nasalization(uncased[i], uncased[i + 1])
-
-        content = ''.join([self.jamo_merge(unc) for unc in uncased])
-        return content
+def nasalization(fc, nc):
+    nasalization_set = {'ㅂㅁ': 'ㅁㅁ', 'ㄷㄴ': 'ㄴㄴ', 'ㄱㅁ': 'ㅇㅁ', 'ㄱㄴ': 'ㅇㄴ', 'ㅇㄹ': 'ㅇㄴ',
+                        'ㅁㄹ': 'ㅁㄴ', 'ㄲㄴ': 'ㅇㄴ', 'ㅂㄹ': 'ㅁㄴ', 'ㄱㄹ': 'ㅇㄴ', 'ㅊㄹ': 'ㄴㄴ',
+                        'ㄺㄴ': 'ㅇㄴ', 'ㅍㄴ': 'ㅁㄴ'}
+    fc_c = fc[-1] + nc[0]
+    if fc_c in nasalization_set:
+        fc[-1], nc[0] = nasalization_set[fc_c]
+    return fc, nc
 
 
-if __name__=='__main__':
-    gen = NoiseGenerator()
+def assimilation(fc, nc):
+    # assimilation not employed in the nasalization function. each other has the similar rules.
+    reverse_assimil = {'ㄺㄴ': 'ㅇㄴ'}
+    fc_c = fc[-1] + nc[0]
+    if fc_c in reverse_assimil:
+        fc[-1], nc[0] = reverse_assimil[fc_c]
+    return fc, nc
+
+
+def phonological_process(content, prob=0.3):
+    uncased = [jamo_split(ch) if re.match('[가-힣]', ch) else [ch, '', ''] for ch in content]
+
+    for i in range(len(uncased) - 1):
+        if random.random() < prob:
+            uncased[i], uncased[i + 1] = palatalization(uncased[i], uncased[i + 1])
+            uncased[i], uncased[i + 1] = linking(uncased[i], uncased[i + 1])
+            uncased[i], uncased[i + 1] = liquidization(uncased[i], uncased[i + 1])
+            uncased[i], uncased[i + 1] = nasalization(uncased[i], uncased[i + 1])
+
+    content = ''.join([jamo_merge(unc) for unc in uncased])
+    return content
+
+
+if __name__ == '__main__':
     sample_text = '행복한 가정은 모두가 닮았지만, 불행한 가정은 모두 저마다의 이유로 불행하다.'
     print('original:', sample_text)
-    print('noised1:', gen.spliting_noise(sample_text, prob=1))
-    print('noised2:', gen.vowel_noise(sample_text, prob=1))
-    print('noised3:', gen.phonological_process(sample_text, prob=1))
+    print('noised1:', splitting_noise(sample_text, prob=1))
+    print('noised2:', vowel_noise(sample_text, prob=1))
+    print('noised3:', phonological_process(sample_text, prob=1))
--- run_noising_text.py
+++ run_noising_text.py
@@ -5,30 +5,36 @@ import logging
 from tqdm import tqdm
 from multiprocessing import Pool, cpu_count
 from functools import partial
-from noising.noise_generator import NoiseGenerator
+from noising.noise_generator import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input_dir', type=str, default=None, required=True, help='Directory of input files')
 parser.add_argument('--output_dir', type=str, default=None, required=True, help='Directory of output files')
-parser.add_argument('--noise_mode', type=str, default='spliting_noise', help='Mode for nosing texts(can be a comma '
+parser.add_argument('--noise_mode', type=str, default='splitting', help='Mode for nosing texts(can be a comma '
                                                                              'seperated)')
 parser.add_argument('--noise_prob', type=float, default=0.1, help='Probability of generating a noise.')
 parser.add_argument('--prefix', type=str, default='', help='Prefix for the output files.')
 parser.add_argument('--delimiter', type=str, default='', help='Delimeter of the units.')
-parser.add_argument('--num_cores', type=str, default=None, help='The number of cpu cores.')
-parser.add_argument('--path_pron', type=str, default='./noising/word_pron_pair.txt',
-                    help='Dictionary path for pronounciation noise.')
 
-ESCAPE = {'\\n': '\n', '\\r': '\r', '\\t': '\t', '\\b': '\b', '\\f': '\f', '\\v': '\v'}
+parser.add_argument('--num_cores', type=str, default=None, help='The number of cpu cores for the multi-process.')
+parser.add_argument('--parallel', type=str, default='intra', help='The way to use the cpu cores for the data'
+                                                                  '("intra" - multiprocessing on the files, '
+                                                                  '"inter" - multiprocessing in one data files).')
 
 
-def run_imap_multiprocessing(func, argument_list, num_processes):
+def run_imap_multi_1(func, argument_list, num_processes, deli=None):
     pool = Pool(processes=num_processes)
+    deli = '' if not deli else deli
+    for result in tqdm(pool.imap(func=func, iterable=argument_list), total=len(argument_list)):
+        contents, path = result
+        write_text(path, deli.join(contents))
 
+
+def run_imap_multi_2(func, argument_list, num_processes):
+    pool = Pool(processes=num_processes)
     result_list_tqdm = []
     for result in tqdm(pool.imap(func=func, iterable=argument_list), total=len(argument_list)):
         result_list_tqdm.append(result)
-
     return result_list_tqdm
 
 
@@ -43,46 +49,59 @@ def write_text(path, content):
         w.write(content)
 
 
-def gen_func(content, funcs, prob):
+def generating_from_string(content, funcs, prob):
     return random.sample(funcs, k=1)[0](content, prob=prob)
 
 
-def gen_func2(input_file, funcs, prob):
-    return random.sample(funcs, k=1)[0](load_text(input_file), prob=prob)
+def generating_from_file(in_out, funcs, deli, prob):
+    _func = random.sample(funcs, k=1)[0]
+    contents = [_func(c, prob=prob) for c in load_text(in_out[0]).split(deli)]
+    return contents, in_out[1]
+
+
+def refine_delimiter(delimiter):
+    ESCAPE = {'\\n': '\n', '\\r': '\r', '\\t': '\t', '\\b': '\b', '\\f': '\f', '\\v': '\v'}
+    delimiter = None if not delimiter else delimiter
+    delimiter = ESCAPE[delimiter] if delimiter in ESCAPE else delimiter
+    return delimiter
+
+
+def get_input_files(path_dir):
+    return [os.path.join(args.input_dir, f) for f in os.listdir(path_dir)]
+
+
+def get_noise_functions(func_names=[]):
+    dict_func = {'splitting': splitting_noise,
+                 'vowel': vowel_noise,
+                 'phonological': phonological_process}
+    return [dict_func[f] for f in func_names if f in dict_func]
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    args.delimiter = None if not args.delimiter else args.delimiter
     logging.basicConfig(level=logging.INFO)
-    num_cores = args.num_cores if args.num_cores else cpu_count()
-    logging.info(f'the number of cpu cores: {num_cores}')
-    input_files = [os.path.join(args.input_dir, fname) for fname in os.listdir(args.input_dir)]
-    generator = NoiseGenerator(args.path_pron)
-    functions = {'spliting_noise': generator.spliting_noise,
-                 'consonant_noise': generator.consonant_noise,
-                 'phonological_noise': generator.phonological_process}
-    logging.info(f'**noise mode: {args.noise_mode}')
-    modes = args.noise_mode.split(',')
 
-    if args.delimiter:
-        func = partial(gen_func, funcs=[functions[m] for m in modes], prob=args.noise_prob)
-        for input_file in input_files:
+    delimiter = refine_delimiter(args.delimiter)
+
+    num_cores = args.num_cores if args.num_cores else cpu_count()
+    logging.info(f'** the number of cpu cores: {num_cores}')
+
+    functions = get_noise_functions(args.noise_mode.split(','))
+    logging.info(f'** noise mode: {args.noise_mode}')
+
+    input_files = get_input_files(args.input_dir)
+    output_files = [os.path.join(args.output_dir, args.prefix + os.path.basename(i)) for i in input_files]
+
+    if args.parallel == 'intra':
+        func = partial(generating_from_file, funcs=functions, deli=delimiter, prob=args.noise_prob)
+        run_imap_multi_1(func, list(zip(input_files,output_files)), num_cores, deli=delimiter)
+    else:
+        func = partial(generating_from_string, funcs=functions, prob=args.noise_prob)
+        for input_file, output_file in zip(input_files, output_files):
             logging.info(f"input file: {input_file}")
             contents = load_text(input_file)
-            args.delimiter = ESCAPE[args.delimiter] if args.delimiter in ESCAPE else args.delimiter
-            contents = contents.split(args.delimiter) if args.delimiter else [contents]
-            noised_texts = run_imap_multiprocessing(func, contents, num_cores)
-            path_output = os.path.join(args.output_dir, args.prefix + os.path.basename(input_file))
-            noised_texts = args.delimiter.join(noised_texts)
-            write_text(path_output, noised_texts)
-            logging.info(f'Saved successfully in {path_output}')
-    else:
-        func = partial(gen_func2, funcs=[functions[m] for m in modes], prob=args.noise_prob)
-        for input_file in input_files:
-            logging.info(f"**input file: {input_file}")
-        noised_texts = run_imap_multiprocessing(func, input_files, num_cores)
-        for content, input_path in zip(noised_texts, input_files):
-            path_output = os.path.join(args.output_dir, args.prefix + os.path.basename(input_path))
-            write_text(path_output, content)
-            logging.info(f'Saved successfully in {path_output}')
+            contents = contents.split(delimiter) if delimiter else [contents]
+            contents = run_imap_multi_2(func, contents, num_cores)
+            contents = args.delimiter.join(contents)
+            write_text(output_file, contents)
+            logging.info(f'Saved successfully in {output_file}')
